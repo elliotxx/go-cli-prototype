413a973b74cf329031b35431f40e5e692fa85232
elliotxx <951376975@qq.com>
2021-12-03 18:53:06 +0800

    feat: support echo option

--- Makefile
+++ Makefile
@@ -1,9 +1,39 @@
 # Default variable
 GOIMPORTS ?= goimports
 GOCILINT ?= golangci-lint
+APP_NAME ?= go-cli-prototype
 
 default:
 	@go run cmd/main.go
+
+build-all: build-darwin build-linux build-windows
+
+build-darwin:
+	-rm -rf ./build/darwin/bin
+	# Update version
+	cd pkg/version/scripts && go run gen.go
+	# Build kusion
+	GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 \
+		go build -o ./build/darwin/bin/$(APP_NAME) \
+		./cmd/main.go
+
+build-linux:
+	-rm -rf ./build/linux/bin
+	# Update version
+	cd pkg/version/scripts && go run gen.go
+	# Build kusion
+	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 \
+		go build -o ./build/linux/bin/$(APP_NAME) \
+		./cmd/main.go
+
+build-windows:
+	-rm -rf ./build/windows/bin
+	# Update version
+	cd pkg/version/scripts && go run gen.go
+	# Build kusion
+	GOOS=windows GOARCH=amd64 CGO_ENABLED=0 \
+		go build -o ./build/windows/bin/$(APP_NAME).exe \
+		./cmd/main.go
 
 test:
 	@go test -timeout=10m `go list ./pkg/... ./cmd/...`
--- cmd/main.go
+++ cmd/main.go
@@ -6,27 +6,54 @@ import (
 
 	"github.com/elliotxx/go-cli-prototype/pkg/version"
 	"github.com/spf13/cobra"
+	"k8s.io/kubectl/pkg/util/i18n"
+	"k8s.io/kubectl/pkg/util/templates"
 )
 
-var showVersion bool
+var (
+	cmdShort   = i18n.T(`A cli application`)
+	cmdLong    = templates.LongDesc(i18n.T(`This is a cli application with go and cobra.`))
+	cmdExample = templates.Examples(i18n.T(`
+		# Show version info
+		go-cli-prototype -V
+		# Show echo info
+		go-cli-prototype -e "hello world"`))
+)
+
+type MainOptions struct {
+	ShowVersion bool
+	Echo        string
+}
+
+// NewEncryptOptions returns a new EncryptOptions instance
+func NewMainOptions() *MainOptions {
+	return &MainOptions{}
+}
 
 func configureCLI() *cobra.Command {
+	o := NewMainOptions()
 	rootCmd := &cobra.Command{
 		Use:          "go-cli-prototype",
-		Long:         "This is a cli application with go and cobra.",
+		Short:        cmdShort,
+		Long:         cmdLong,
+		Example:      cmdExample,
 		SilenceUsage: true,
-		Example:      "go-cli-prototype -h",
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			// Show version info
-			if showVersion {
+			if o.ShowVersion {
 				fmt.Println(version.ReleaseVersion())
+				return nil
+			}
+			if len(o.Echo) > 0 {
+				fmt.Println(o.Echo)
 				return nil
 			}
 			return nil
 		},
 	}
 
-	rootCmd.Flags().BoolVarP(&showVersion, "version", "V", false, "Show version info")
+	rootCmd.Flags().BoolVarP(&o.ShowVersion, "version", "V", false, "Show version info")
+	rootCmd.Flags().StringVarP(&o.Echo, "echo", "e", "", "Show echo info")
 	return rootCmd
 }
 
